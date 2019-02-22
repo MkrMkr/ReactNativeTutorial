c53be3b8bdf99536084cb6010dbfe616a2a50feb
Mirosław Król <mkrol@concisesoftware.com>
2019-02-22 12:31:51 +0100

    ESLint setup

--- app/App.js
+++ app/App.js
@@ -4,9 +4,9 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./components/Home/Home.component";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Home from './components/Home/Home.component';
 
 class NoteTaker extends Component {
   render() {
@@ -17,20 +17,20 @@ class NoteTaker extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
-    margin: 10
+    textAlign: 'center',
+    margin: 10,
   },
   instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
 export default NoteTaker;
