2fd1b945519d8b81f7eb539c8b53f8ac959a736d
Mirosław Król <mkrol@concisesoftware.com>
2019-02-22 07:59:26 +0100

    Moved App.js to app folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import { AppRegistry } from "react-native";
-import NoteTaker from "./App";
+import NoteTaker from "./app/App";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => NoteTaker);
