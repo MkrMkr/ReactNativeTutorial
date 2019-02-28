53b3f9c10157f937c7bad914e05ba97eff03487f
Mirosław Król <mkrol@concisesoftware.com>
2019-02-28 14:27:33 +0100

    Added default theme values, sample styles, updated values in TextArea.component.style.js to these from theme.styles

---
+++ app/styles/styles.js
@@ -0,0 +1,15 @@
+import theme from "./theme.style";
+
+export default StyleSheet.create({
+  largeButtonText: {
+    fontSize: theme.FONT_SIZE_LARGE,
+    fontWeight: theme.FONT_WEIGHT_HEAVY
+  },
+  largeHeaderText: {
+    fontSize: theme.FONT_SIZE_LARGE
+  },
+  mediumHeaderText: {
+    fontSize: theme.FONT_SIZE_MEDIUM,
+    color: theme.PRIMARY_COLOR
+  }
+});
