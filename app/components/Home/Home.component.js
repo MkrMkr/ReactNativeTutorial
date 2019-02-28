bba599c4c860c7251e20c3a79447b0e2d0cc422d
Mirosław Król <mkrol@concisesoftware.com>
2019-02-28 14:42:40 +0100

    Home.component styling

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
-import {TextInput, View, Text} from 'react-native';
-import PropTypes from 'prop-types'; // TODO: nieuzywany import?
-import styles from './Home.component.style';
-import TextArea from '../TextArea/TextArea.component';
+import React, { Component } from "react";
+import { TextInput, View, Text } from "react-native";
+import PropTypes from "prop-types"; // TODO: nieuzywany import?
+import styles from "./Home.component.style";
+import TextArea from "../TextArea/TextArea.component";
 
 class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> Please enter your note here </Text>
-        <TextArea />
+        <Text style={styles.textAreaTitle}> Please enter your note here </Text>
+        <TextArea style={styles.textArea} />
       </View>
     );
   }
