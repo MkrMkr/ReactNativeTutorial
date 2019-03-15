16069017ed8f3651789b9a5e62adb19f34e416bb
mkrol <mkrol@concisesoftware.com>
2019-03-15 12:25:34 +0100

    GeoServ with missing PermissionAndroid

--- app/components/GeoServ/GeoServ.component.js
+++ app/components/GeoServ/GeoServ.component.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { PermissionAndroid, View, Text } from "react-native";
+import Geolocation from "react-native-geolocation-service";
 
 export default class GeoServ extends Component {
+  async componentDidMount() {
+    // Instead of navigator.geolocation, just use Geolocation.
+    const hasLocationPermission = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "App needs to access your location",
+        message:
+          "App needs access to your location " +
+          "so we can let our app be even more awesome."
+      }
+    );
+    if (hasLocationPermission) {
+      Geolocation.getCurrentPosition(
+        position => {
+          console.log(position);
+        },
+        error => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+    }
+  }
+
   render() {
     const { ...extraProps } = this.props; // TODO: co te 3 kropki tu znacza? to jest jakas tablica czy coś?
     return <Text>Abcd</Text>;
