a778cbd066c79a8f8b54587740da17fb099cfde2
mkrol <mkrol@concisesoftware.com>
2019-03-15 12:33:15 +0100

    PermissionsAndroid import

--- app/components/GeoServ/GeoServ.component.js
+++ app/components/GeoServ/GeoServ.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { PermissionAndroid, View, Text } from "react-native";
+import { PermissionsAndroid, View, Text } from "react-native";
 import Geolocation from "react-native-geolocation-service";
 
 export default class GeoServ extends Component {
