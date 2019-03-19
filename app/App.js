da5dcb693241520ff5941ba28d1a8bf7d7f7b50b
mkrol <mkrol@concisesoftware.com>
2019-03-19 09:42:06 +0100

    Added GeoSample screen

--- app/App.js
+++ app/App.js
@@ -1,9 +1,11 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "./components/Home/Home.component";
+import GeoSample from "./components/GeoSample/GeoSample.component";
 
 const AppNavigator = createStackNavigator(
   {
-    Home: Home
+    Home: Home,
+    GeoSample: GeoSample
   },
   {
     initialRouteName: "Home"
