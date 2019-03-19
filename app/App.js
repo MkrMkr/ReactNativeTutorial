c122bfa50da424471930b10b26a0b4664042a4a6
mkrol <mkrol@concisesoftware.com>
2019-03-19 09:08:38 +0100

    Initial component

--- app/App.js
+++ app/App.js
@@ -1,36 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "./components/Home/Home.component";
 
-class NoteTaker extends Component {
-  render() {
-    return <Home />;
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  }
+const AppNavigator = createStackNavigator({
+  screen: Home
 });
 
-export default NoteTaker;
+export default createAppContainer(AppNavigator);
