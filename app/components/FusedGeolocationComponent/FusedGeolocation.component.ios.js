f8294135cdc0e68d89a2472616030b606108cee6
mkrol <mkrol@concisesoftware.com>
2019-03-20 14:01:06 +0100

    Refactored default geolocaiton implementation

--- app/components/FusedGeolocationComponent/FusedGeolocation.component.ios.js
+++ app/components/FusedGeolocationComponent/FusedGeolocation.component.ios.js
@@ -15,31 +15,22 @@ export default class FusedGeolocation extends Component {
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log(
-          "New position in Geolocation component: " +
-            position.coords.latitude +
-            "," +
-            position.coords.longitude
+        alert(
+          "Default RN geolocation (used for iOS), getCurrentPosition (success)"
         );
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           timestamp: position.timestamp
         });
-        console.log(
-          "geolocation comp latitude: " +
-            this.latitude +
-            " longitude: " +
-            this.longitude
-        );
       },
       error => {
-        console.log(error);
+        "Default RN geolocation (used for iOS), getCurrentPosition (error)";
       },
       { enableHighAccuracy: true, timeout: 30000 }
     );
     this.watchId = navigator.geolocation.watchPosition(newPosition => {
-      alert("newPosition:" + newPosition);
+      alert("Default RN geolocation (used for iOS), watchPosition");
     });
   }
 
@@ -49,7 +40,7 @@ export default class FusedGeolocation extends Component {
 
   render() {
     return (
-      <View>
+      <View style={{ flex: 1, alignItems: "center" }}>
         <Text>---RN default geolocation---</Text>
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
