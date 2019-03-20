26640aa9e7b7c80f215a74e55a9660594cf0e34f
mkrol <mkrol@concisesoftware.com>
2019-03-20 13:07:30 +0100

    Added SafeAreaView

--- app/components/Home/Home.component.js
+++ app/components/Home/Home.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, TextInput, View, Text } from "react-native";
+import { SafeAreaView, Button, TextInput, View, Text } from "react-native";
 import styles from "./Home.component.style";
 import TextArea from "../TextArea/TextArea.component";
 import CustomButton from "../Button/CustomButton.component";
@@ -23,23 +23,28 @@ class Home extends Component {
   setTitle = title => this.setState({ title }); //TODO: Ogarnąc tę konstrukcję
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.titleHeading}> Note Title</Text>
-        <TextInput
-          style={styles.titleTextInput}
-          onChangeText={this.setTitle}
-          value={this.state.title}
-        />
-        <Text style={styles.textAreaTitle}> Please enter your note here </Text>
-        <TextArea style={styles.textArea} />
-        <View style={{ flexDirection: "row" }}>
-          <CustomButton onPress={() => this.goToGeoServAction()} />
-          <Button
-            title="Go to FusedGeolocation sample"
-            onPress={() => this.props.navigation.navigate("FusedGeolocation")}
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={styles.titleHeading}> Note Title</Text>
+          <TextInput
+            style={styles.titleTextInput}
+            onChangeText={this.setTitle}
+            value={this.state.title}
           />
+          <Text style={styles.textAreaTitle}>
+            {" "}
+            Please enter your note here{" "}
+          </Text>
+          <TextArea style={styles.textArea} />
+          <View style={{ flexDirection: "row" }}>
+            <CustomButton onPress={() => this.goToGeoServAction()} />
+            <Button
+              title="Go to FusedGeolocation sample"
+              onPress={() => this.props.navigation.navigate("FusedGeolocation")}
+            />
+          </View>
         </View>
-      </View>
+      </SafeAreaView>
     );
   }
 }
