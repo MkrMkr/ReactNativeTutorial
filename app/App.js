28e3d0d03a51043dea5980a930a95e6c83fd4b0f
mkrol <mkrol@concisesoftware.com>
2019-03-25 07:48:53 +0100

    Removed GeoSample

--- app/App.js
+++ app/App.js
@@ -1,6 +1,5 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "./components/Home/Home.component";
-import GeoSample from "./components/GeoSample/GeoSample.component";
 import FusedGeolocation from "./components/FusedGeolocationComponent/FusedGeolocation.component";
 import GeoServImproved from "./components/GeoServImproved/GeoServImproved.component";
 import ReactNariveGeoServ from "./components/GeoServ/ReactNativeGeoServ";
@@ -8,7 +7,6 @@ import ReactNariveGeoServ from "./components/GeoServ/ReactNativeGeoServ";
 const AppNavigator = createStackNavigator(
   {
     Home: Home,
-    GeoSample: GeoSample,
     FusedGeolocation: FusedGeolocation,
     ReactNativeGeoServ: ReactNariveGeoServ,
     GeoServImproved: GeoServImproved
