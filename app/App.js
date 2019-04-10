22a4ba3ce7018bf2b6a37e29f003cee7840181ff
Mirosław Król <mkrol@concisesoftware.com>
2019-04-10 08:35:30 +0200

    Small updates

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "./components/Home/Home.component";
 import FusedGeolocation from "./components/FusedGeolocationComponent/FusedGeolocation.component";
 import ReactNariveGeoServ from "./components/GeoServ/ReactNativeGeoServ";
-import ReactNativeLocation from "./components/GeoServImproved/ReactNativeLocation.component";
+import ReactNativeLocation from "./components/ReactNativeLocation/ReactNativeLocation.component";
 
 const AppNavigator = createStackNavigator(
   {
